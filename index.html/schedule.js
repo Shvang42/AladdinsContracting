1e0e31e619ef861666acf9d6dfa26926c3a275a4
OsmanElias <93014594+OsmanElias@users.noreply.github.com>
2022-04-26 15:32:32 -0400

    Schedule Update
    Updated

--- index.html/schedule.js
+++ index.html/schedule.js
@@ -2,7 +2,9 @@
 
 function sendEmail(){
     Email.send({
-    SecureToken : "de236c8f-8eb3-45dc-a07c-ef632176aec2",
+    Host : "smtp.gmail.com",
+    Username : "aladdingscontracting@gmail.com",
+    Password : "kdpyntzdhopmjkih",
     To : 'aladdingscontracting@gmail.com',
     From : document.getElementById("email").value,
     Subject : " Appointment Inquiry",
