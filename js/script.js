3c1bff189274b4c7cb04e9f16bc6c0ea182e6840
OsmanElias <93014594+OsmanElias@users.noreply.github.com>
2024-02-10 14:30:42 -0500

    Reworked entire site for improved UX
    - Reworked site to enhance visual appeal and user engagement.
    - Updated responsive design settings for better performance across all device sizes.
    - Refactored CSS for cleaner and more maintainable code.
    
    These changes aim to provide a more immersive experience for visitors and ensure a consistent look and feel across devices. As well as vault the older and out of date site

---
+++ js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * Add event listener on multiple elements
+ */
+const addEventOnElements = (elements, eventType, callback) => {
+  elements.forEach(element => {
+    element.addEventListener(eventType, callback);
+  });
+};
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = () => {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * HEADER
+ * Active header when window scrolls down to 100px
+ */
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("active", window.scrollY > 100);
+});
+
+/**
+ * SLIDER
+ */
+document.querySelectorAll("[data-slider]").forEach(currentSlider => {
+  const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = () => {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+  };
+
+  sliderNextBtn.addEventListener("click", () => {
+    currentSlidePos = currentSlidePos >= sliderContainer.childElementCount - 1 ? 0 : currentSlidePos + 1;
+    moveSliderItem();
+  });
+
+  sliderPrevBtn.addEventListener("click", () => {
+    currentSlidePos = currentSlidePos <= 0 ? sliderContainer.childElementCount - 1 : currentSlidePos - 1;
+    moveSliderItem();
+  });
+
+  if (sliderContainer.childElementCount <= 1) {
+    sliderNextBtn.style.display = "none";
+    sliderPrevBtn.style.display = "none";
+  }
+});
+
+/**
+ * ACCORDION
+ */
+let lastActiveAccordion = null;
+
+document.querySelectorAll("[data-accordion]").forEach(currentAccordion => {
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
+
+  accordionBtn.addEventListener("click", () => {
+    if (lastActiveAccordion && lastActiveAccordion !== currentAccordion) {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+    currentAccordion.classList.toggle("expanded");
+    lastActiveAccordion = currentAccordion;
+  });
+});
